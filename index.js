dfd4b10b69b991bb7ca2ac82ba2bae3f65f2e3b2
000angelica <121267408+000angelica@users.noreply.github.com>
2023-02-01 18:29:38 -0600

    clean up code, change css

--- index.js
+++ index.js
@@ -37,22 +37,34 @@ currentDate.innerHTML = `${day} ${month} ${date}, ${hours}:${minutes}`;
 
 //
 function displayWeather(response) {
-  document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.main.temp
+  console.log("response", response);
+  document.querySelector("#city").innerHTML = response.data.city;
+  document.querySelector("#current-temperature").innerHTML = Math.round(
+    response.data.temperature.current
   );
-  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#humidity").innerHTML =
+    response.data.temperature.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
+
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+
+  iconElement.setAttribute("alt", response.data.condition.icon);
+
   document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
+    response.data.condition.description;
 }
 
 function searchCity(city) {
   let apiKey = "4368ot5e9149d5a5bd05b09133f063bf";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Minneapolis&key={key}`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeather);
+  console.log();
 }
 
 function handleSubmit(event) {
@@ -63,7 +75,7 @@ function handleSubmit(event) {
 
 function searchLocation(position) {
   let apiKey = "4368ot5e9149d5a5bd05b09133f063bf";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?lon={lon}&lat={lat}&key={key}`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?lon={lon}&lat={lat}&key={key}&units=metric`;
   axios.get(apiUrl).then(showWeather);
 }
 
@@ -74,12 +86,12 @@ function getCurrentLocation(event) {
 
 function convertToCelsius(event) {
   event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
+  let temperatureElement = document.querySelector(".unitsCelsius");
   temperatureElement.innerHTML = -4;
 }
 function convertToFarenheit(event) {
   event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
+  let temperatureElement = document.querySelector(".unitsFarenheit");
   temperatureElement.innerHTML = 24;
 }
 let searchForm = document.querySelector("#search-form-id");
